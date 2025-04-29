c94db212088932ca133bfe8a5b799259f772ab90
Bimai6 <bimaivfx.gmail.com>
2025-04-29 14:02:22 +0200

    feat: add show and update functions in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,23 +12,29 @@ use Symfony\Component\HttpFoundation\Request;
 
 final class UserController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+
     #[Route('/user/create_user', name: 'create_user')]
-    public function createUser(EntityManagerInterface $entityManager): Response{
+    public function createUser(): Response{
         $user = new User();
         $user->setName('Javi');
         $user->setSurname('Ariza Rosales');
         $user->setAge(21);
         $user->setDni('12345678A');
 
-        $entityManager->persist($user);
+        $this->em->persist($user);
 
-        $entityManager->flush();
+        $this->em->flush();
 
         return new Response('Saved new user with id ' . $user->getId());
     }
 
     #[Route('user/form', name:'app_form'), ]
-    public function processForm(Request $request, EntityManagerInterface $entityManager): Response {
+    public function processForm(Request $request): Response {
         
         $user = new User();
 
@@ -39,20 +44,54 @@ final class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
 
-            $entityManager->persist($user);
+            $this->em->persist($user);
 
-            $entityManager->flush();
+            $this->em->flush();
 
             return $this->redirectToRoute('user_success');
         }
 
         return $this->render('user/form.html.twig', [
-            'form' => $form,
+            'form' => $form
         ]);
     }
 
     #[Route('user/confirmed', name:'user_success')]
     public function index(){
         return $this->render('user/success.html.twig');
+    }
+
+    #[Route('user/{id}', name:'user_show')]
+    public function show(int $id): Response {
+        $user = $this->em->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great user: '.$user->getName());
+    }
+
+    #[Route('user/edit/{id}', name:'user_update')]
+    public function update(Request $request, int $id) : Response{
+        $user= $this->em->getRepository(User::class)->find($id);
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $this->em->flush();
+
+            return new Response('User with id ' . $id . ' has been update!');
+        }
+
+        return $this->render('user/update.html.twig', [
+            'form' => $form,
+            'username' => $user->getName()
+        ]);
     }
 }
