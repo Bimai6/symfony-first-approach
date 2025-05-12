204138e344c7318dba7588facbac73931687949e
Bimai6 <bimaivfx@gmail.com>
2025-05-12 19:52:14 +0200

    feat: Add hasher to UserCrudController

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -7,12 +7,29 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+
+    private UserPasswordHasherInterface $passwordHasher;
+
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof User) return;
+
+        if ($entityInstance->getPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword());
+            $entityInstance->setPassword($hashedPassword);
+        }
+
+        parent::persistEntity($entityManager, $entityInstance);
     }
 
     /*
